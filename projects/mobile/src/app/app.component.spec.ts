078117360bceafcc1b0306d6f3118474212de582
ropoko <rodrigostramantinoli@gmail.com>
2023-01-02 15:14:45 -0300

    refactor: multi projects

--- projects/mobile/src/app/app.component.spec.ts
+++ projects/mobile/src/app/app.component.spec.ts
@@ -1,13 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
@@ -20,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tuneradio'`, () => {
+  it(`should have as title 'mobile'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tuneradio');
+    expect(app.title).toEqual('mobile');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tuneradio app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('mobile app is running!');
   });
 });
