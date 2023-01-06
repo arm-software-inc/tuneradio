584de2f1f1e4c5ee5147861eb7f8271f44427a1c
murillo.nahas <murillonahas@hotmail.com>
2023-01-06 14:37:18 -0300

    feat: run lint and core components

---
+++ projects/base/src/lib/components/header-web/header-web.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'base-header-web',
+	templateUrl: './header-web.component.html',
+	styleUrls: ['./header-web.component.css']
+})
+export class HeaderWebComponent {
+
+}
